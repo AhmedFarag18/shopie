ce08510a980769dba0ec5755f1a20ee04e8fb5e9
AhmedFarag18 <ahmedfarag11_sd@fcis.bsu.edu.eg>
2025-06-05 15:41:23 +0300

    enhance layout structure

--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,22 +1,16 @@
+import Footer from "./Footer"
+import Navbar from "./Navbar"
 
 function MainLayout({ children }) {
   return (
     <div className='main-layout'>
-      <div className='navbar'>
-        Add Navbar Component Here
-      </div>
-
-      <div className="card bg-light p-3 m-5 rounded-md w-[300px] h-[300px] font-heading shadow-md text-text-primary">
-        Card 1 Lorem, ipsum.
-      </div>
+      <Navbar />
 
       <main>
         {children}
       </main>
 
-      <div className='footer'>
-        Add Footer Component Here
-      </div>
+      <Footer />
     </div>
   )
 }
