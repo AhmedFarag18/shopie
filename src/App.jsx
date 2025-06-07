aae10436fea7f0890e15195c86596f6de0375b05
Haneen Khalil <haneenkhalil248@gmail.com>
2025-06-07 18:26:07 +0300

    Add initial Navbar CSS file for styling the navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,21 @@ import { Toaster } from "react-hot-toast";
 import { routes } from "./routes/routing";
 import MainLayout from "./components/layout/MainLayout";
 
-
 function App() {
   return (
     <>
       <Toaster />
       <BrowserRouter>
         <MainLayout>
-          <Routes>
-            {routes.map((route, i) => (
-              <Route key={i} path={route.path} element={route.element} />
-            ))}
-          </Routes>
+          
+            <Routes>
+              {routes.map((route, i) => (
+                <Route key={i} path={route.path} element={route.element} />
+              ))}
+            </Routes>
         </MainLayout>
       </BrowserRouter>
+
     </>
   );
 }
--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,6 +1,5 @@
 import Footer from "./Footer"
 import Navbar from "./Navbar"
-
 function MainLayout({ children }) {
   return (
     <div className='main-layout'>
