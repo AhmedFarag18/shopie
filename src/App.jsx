e899eaa9ec14be64adbe0f0bad07158e57024d00
Ahmed Farag <73128333+AhmedFarag18@users.noreply.github.com>
2025-06-08 03:10:18 +0300

    Update unUsed Components in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import { Toaster } from "react-hot-toast";
 import { routes } from "./routes/routing";
 import MainLayout from "./components/layout/MainLayout";
-import HeroSlider from "./components/common/HeroSlider";
-import ExploreCategories from "./components/common/ExploreCategories";
-import DealOfTheDay from "./components/common/DealOfTheDay";
+
 
 function App() {
   return (
@@ -12,12 +10,6 @@ function App() {
       <Toaster />
       <BrowserRouter>
         <MainLayout>
-           <HeroSlider />
-      <ExploreCategories />
-      <DealOfTheDay />
-      <div className="min-h-screen bg-[#f9f9f9] font-sans"> </div>
-      <div className="fixed right-0 bottom-10 rotate-90 text-xs text-gray-400">Scroll Page</div>
-
             <Routes>
               {routes.map((route, i) => (
                 <Route key={i} path={route.path} element={route.element} />
